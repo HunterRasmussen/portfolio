e9e8cb3fc11efb47571812038a8c498c320d7330
Hunter Rasmussen <hunterhrasmussen@gmail.com>
2018-08-03 11:43:14 -0600

    Tried to start building fireflies.

--- vue.js
+++ vue.js
@@ -11,6 +11,18 @@ new Vue({
     hoverMenu: false,
     showMenu: false,
 
+    //firefly stuff
+    canvas: '',
+    maxFlies: 15,
+    flyXSpeedRange: [-1,1],
+    flyYSpeedRange: [-0.5,0.5],
+    flySizeRange: [1,5],
+    flyLifespanRange: [75,150],
+    currentFlies: [],
+
+
+
+
   },
 
   methods: {
@@ -28,12 +40,31 @@ new Vue({
         this.$refs.menuItems.id = "menuItems";
       }
 
-      console.log(this.$refs.myMenu.id)
+      console.log(this.$refs);
 
     },
 
-  }
+    getRandomRange: function(min,max){
+        return Math.random() * (max - min) + min;
+    },
 
+    fly: function(options){
+        if(!options){
+          
+        }
+    },
+
+    createFlies: function(){
+      if(this.currentFlies.length < maxFlies){
+        currentFlies.push(new fly());
+      }
+    }
+
+  },
+
+  mounted: function(){
+      this.canvas = this.$refs.canvas;
+  }
 
 
 
