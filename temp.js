32983a3550df835ef5ba42e6569ce6496f05fc56
Hunter <hunterhrasmussen@gmail.com>
2019-07-09 20:00:05 -0600

    Still working on sticky

--- temp.js
+++ temp.js
@@ -16,3 +16,25 @@ new Vue({
 
 
 })
+
+const $marker = document.querySelector("#oneTop");
+const $stickyElement = document.querySelector("#one");
+
+const stickyHeader = () => function() {
+  const sr1 = $marker.getBoundingClientRect();
+  const sr2 = $stickyElement.getBoundingClientRect();
+
+  if (sr1.top > 0) { //marker has reached top
+    $stickyElement.classList.remove("sticky");
+    $marker.style.top = 0;
+  } else {
+    $stickyElement.classList.add("sticky");
+    $marker.style.top = -1;
+  }
+};
+
+window.addEventListener("scroll",  stickyHeader()); //_.debounce(stickyHeader(), 15) );
+
+
+
+
