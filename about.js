9e729213ca4a1e3362f609419b517e0bc73a28bc
Video <video@Videos-iMac.local>
2020-03-12 15:14:16 -0600

    Added content to About.html and fixed some anims.

--- about.js
+++ about.js
@@ -12,3 +12,22 @@ $(document).on("scroll", function () {
   }
 }
 })
+
+$(document).ready(function(){
+    var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height() * 7/8
+  var tags = $("section") 
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    if ($(tag).position().top < pageBottom) { 
+      $(tag).addClass("visible")
+    }
+  }   
+
+  tags = $(".loadin")
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    $(tag).addClass("visible")
+  }
+});
